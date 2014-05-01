959052294e0c7fcb203473fa26d0871a4ff01fe5
Joe21 <joej21@gmail.com>
2014-05-01 19:42:46 -0400

    seeded teams in controller for now

--- js/angular/js/controllers.js
+++ js/angular/js/controllers.js
@@ -1,1 +1,130 @@
-controllers.js
+var myApp = angular.module('myApp', []);
+
+myApp.controller('MyController', function MyController($scope){
+	$scope.teams = [
+		{
+			"name":"Arizona Cardinals",
+			"shortname":"Arizona_Cardinals"
+		},
+		{
+			"name":"Atlanta Falcons",
+			"shortname":"Atlanta_Falcons"
+		},
+		{
+			"name":"Baltimore Ravens",
+			"shortname":"Baltimore_Ravens"
+		},
+		{
+			"name":"Buffalo Bills",
+			"shortname":"Buffalo_Bills"
+		},
+		{
+			"name":"Carolina Panthers",
+			"shortname":"Carolina_Panthers"
+		},
+		{
+			"name":"Chicago Bears",
+			"shortname":"Chicago_Bears"
+		},
+		{
+			"name":"Cleveland Browns",
+			"shortname":"Cleveland_Browns"
+		},
+		{
+			"name":"Dallas Cowboys",
+			"shortname":"Dallas_Cowboys"
+		},
+		{
+			"name":"Denver Broncos",
+			"shortname":"Denver_Broncos"
+		},
+		{
+			"name":"Detroit Lions",
+			"shortname":"Detroit_Lions"
+		},
+		{
+			"name":"Green Bay Packers",
+			"shortname":"Green_Bay_Packers"
+		},
+		{
+			"name":"Houston Texans",
+			"shortname":"Houston_Texans"
+		},
+		{
+			"name":"Indianapolis Colts",
+			"shortname":"Indianapolis_Colts"
+		},
+		{
+			"name":"Jacksonville Jaguars",
+			"shortname":"Jacksonville_Jaguars"
+		},
+		{
+			"name":"Kansas City Chiefs",
+			"shortname":"Kansas_City_Chiefs"
+		},
+		{
+			"name":"Miami Dolphins",
+			"shortname":"Miami_Dolphins"
+		},
+		{
+			"name":"Minnesota Vikings",
+			"shortname":"Minnesota_Vikings"
+		},
+		{
+			"name":"New England Patriots",
+			"shortname":"New_England_Patriots"
+		},
+		{
+			"name":"New Orleans Saints",
+			"shortname":"New_Orleans_Saints"
+		},
+		{
+			"name":"New York Giants",
+			"shortname":"New_York_Giants"
+		},
+		{
+			"name":"New York Jets",
+			"shortname":"New_York_Jets"
+		},
+		{
+			"name":"Oakland Raiders",
+			"shortname":"Oakland_Raiders"
+		},
+		{
+			"name":"Philadelphia Eagles",
+			"shortname":"Philadelphia_Eagles"
+		},
+		{
+			"name":"Pittsburgh Steelers",
+			"shortname":"Pittsburgh_Steelers"
+		},
+		{
+			"name":"San Diego Chargers",
+			"shortname":"San_Diego_Chargers"
+		},
+		{
+			"name":"San Francisco 49ers",
+			"shortname":"San_Francisco_49ers"
+		},
+		{
+			"name":"Seattle Seahawks",
+			"shortname":"Seattle_Seahawks"
+		},
+		{
+			"name":"St. Louis Rams",
+			"shortname":"St_Louis_Rams"
+		},
+		{
+			"name":"Tampa Bay Buccaneers",
+			"shortname":"Tampa_Bay_Buccaneers"
+		},
+		{
+			"name":"Tennessee Titans",
+			"shortname":"Tennessee_Titans"
+		},
+		{
+			"name":"Washington Redskins",
+			"shortname":"Washington_Redskins"
+		}
+	];
+});
